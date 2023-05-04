15b41be7e5cfb97a6fa5530adb2d3f583bc1d3d4
Jorge <jorge.villalobos@premise.com>
2023-05-04 11:27:08 -0400

    feat: static site generation

--- adapters/netlify-edge/vite.config.ts
+++ adapters/netlify-edge/vite.config.ts
@@ -1,16 +1,23 @@
-import { netlifyEdgeAdapter } from '@builder.io/qwik-city/adapters/netlify-edge/vite';
-import { extendConfig } from '@builder.io/qwik-city/vite';
-import baseConfig from '../../vite.config';
+import { netlifyEdgeAdapter } from "@builder.io/qwik-city/adapters/netlify-edge/vite";
+import { extendConfig } from "@builder.io/qwik-city/vite";
+import baseConfig from "../../vite.config";
 
 export default extendConfig(baseConfig, () => {
   return {
     build: {
       ssr: true,
       rollupOptions: {
-        input: ['src/entry.netlify-edge.tsx', '@qwik-city-plan'],
+        input: ["src/entry.netlify-edge.tsx", "@qwik-city-plan"],
       },
-      outDir: '.netlify/edge-functions/entry.netlify-edge',
+      outDir: ".netlify/edge-functions/entry.netlify-edge",
     },
-    plugins: [netlifyEdgeAdapter()],
+    plugins: [
+      netlifyEdgeAdapter({
+        staticPaths: ["/"],
+        ssg: {
+          include: ["/"],
+        },
+      }),
+    ],
   };
 });
